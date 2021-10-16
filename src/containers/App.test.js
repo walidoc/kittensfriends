da715e8a438241d7221f10bf8797cc57d719879e
Giraficode <giraficode@gmail.com>
2021-10-16 14:30:51 +0000

    add test

---
+++ src/containers/App.test.js
@@ -0,0 +1,17 @@
+const App = require("./App")
+// @ponicode
+describe("componentDidMount", () => {
+    let inst
+
+    beforeEach(() => {
+        inst = new App.default()
+    })
+
+    test("0", () => {
+        let callFunction = () => {
+            inst.componentDidMount()
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
